a51d17c53b18de06357ea1a43f74de35bec68b3e
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-06-25 02:55:26 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,29 +2,36 @@
 
 set -e
 
+# Run inside foss.crave.io devspace, in the project folder
 crave run --no-patch -- "
-  rm -rf .repo/local_manifests &&
+# Remove existing local_manifests
+rm -rf .repo/local_manifests &&
 
-  repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs &&
+# Initialize repo with specified manifest
+repo init -u https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs &&
 
-  git clone https://github.com/Novicio-2309/local_manifests.git -b pixelage-bp1a-pova4series .repo/local_manifests &&
+# Clone your local_manifests
+git clone https://github.com/Novicio-2309/local_manifests.git -b pixelage-bp1a-pova4series .repo/local_manifests &&
 
-  rm -rf vendor/lineage-priv/keys &&
+# Remove test keys (optional if you want to avoid testkey conflicts)
+rm -rf vendor/lineage-priv/keys &&
 
-  /opt/crave/resync.sh &&
+# Download and extract your signing keys from PixelDrain
+curl -L -o signing-keys.tar.gz https://pixeldrain.com/api/file/VBFj5hBW &&
+tar -xvzf signing-keys.tar.gz &&
 
-  PIXELAGE_BUILD=LG7n &&
-  source build/envsetup.sh &&
+# Start repo sync
+/opt/crave/resync.sh &&
 
-  lunch pixelage_LG7n-bp1a-userdebug &&
+# Set up environment
+PIXELAGE_BUILD=LG7n &&
+source build/envsetup.sh &&
+lunch pixelage_LG7n-bp1a-userdebug &&
 
-  mka target-files-package otatools &&
-
-  /opt/crave/crave_sign.sh &&
-
-  mka bacon
+# Start the build
+mka bacon
 "
 
-# Pull ROM ZIP and images
+# Pull output files
 crave pull out/target/product/*/*.zip
 crave pull out/target/product/*/*.img
