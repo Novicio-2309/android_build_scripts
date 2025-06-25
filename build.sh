c097319c2bbe83574f09cf1f4aa1c79d51ad3086
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-06-25 21:15:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 set -e
 
-# Run inside foss.crave.io devspace, in the project folder
+# Build sa loob ng Crave devspace
 crave run --no-patch -- " \
-    # Remove existing local_manifests
+    # Linisin ang lumang manifest at soong
     rm -rf .repo/local_manifests; \
     rm -rf build/soong; \
-    rm -rf vendor/lineage-priv/keys; \
     \
-    # Initialize repo with specified manifest
+    # Repo init gamit ang InfinityX manifest
     repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault; \
     \
-    # Clone your local_manifests
+    # Clone ng local manifests mo
     git clone https://github.com/Novicio-2309/local_manifests.git -b infinityX-15-bp1a .repo/local_manifests; \
     \
-    # Signingkey for infinity
+    # Clone ng signing keys at pag-generate ng keys
     git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys; \
-    cd vendor/infinity-priv/keys; \
-    ./keys.sh; \
+    cd vendor/infinity-priv/keys && chmod +x keys.sh && ./keys.sh; \
+    cd ../../../; \
     \
-    # Start repo sync
+    # Sync ng source code
     /opt/crave/resync.sh; \
     \
-    # Set up environment
+    # Setup ng environment
     . build/envsetup.sh; \
     lunch infinity_LG7n-userdebug; \
     \
-    # Start the build
+    # Simulan ang build
     mka bacon \
 "
