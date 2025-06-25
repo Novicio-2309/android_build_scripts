b7e6bc0148c1387502d79e7c6faa9a44502e6b0a
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-06-25 16:25:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ crave run --no-patch -- "
 # Remove existing local_manifests
 rm -rf .repo/local_manifests &&
 rm -rf build/soong &&
-rm -rf vendor/lineage-priv/keys
+rm -rf vendor/lineage-priv/keys &&
 
 # Initialize repo with specified manifest
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault &&
