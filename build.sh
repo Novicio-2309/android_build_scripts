ed8a7e1a3ed777adc8626c062b64ae0700cb5c5f
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-07-02 14:28:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,20 @@ set -e
 crave run --no-patch -- "
   # 🔹 Hakbang 1: Linisin ang mga lumang manifest, soong, at keys
   rm -rf .repo/local_manifests && \
+  rm -rf device/tecno/LG7n && \
+  rm -rf device/tecno/mt6789-common && \
+  rm -rf device/tecno/LG7n-kernel && \
+  rm -rf vendor/tecno/LG7n && \
+  rm -rf vendor/tecno/mt6789-common && \
+  rm -rf vendor/sony/dolby && \
+  rm -rf packages/apps/ViPER4AndroidFX && \
+  rm -rf hardware/mediatek && \
+  rm -rf hardware/transsion && \
+  rm -rf device/mediatek/sepolicy_vndr && \
+  rm -rf vendor/alpha-priv/keys && \
+  rm -rf build/soong && \
+  rm -rf vendor/google/gms && \
+  rm -rf vendor/gms && \
 
   # 🔹 Hakbang 2: I-repo init gamit ang InfinityX manifest
   repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs && \
@@ -11,16 +25,13 @@ crave run --no-patch -- "
   # 🔹 Hakbang 3: I-clone ang local manifests
   git clone https://github.com/Novicio-2309/local_manifests.git -b evox15-bp2a .repo/local_manifests && \
 
+# signingkey Evox default
+git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys && \
+cd vendor/evolution-priv/keys && \
+./keys.sh && \
+
   # 🔹 Hakbang 7: I-sync ang buong source
   /opt/crave/resync.sh && \
-
-  # 🔹 Hakbang 6: Alisin ang Chrome mula sa GMS
-  echo '➤ Inaalis ang Chrome...' && \
-  sed -i '/Chrome.apk/s/^/# /' vendor/google/gms/proprietary-files.txt || true && \
-  sed -i '/Chrome/s/^/# /' vendor/google/gms/gms-vendor.mk || true && \
-  rm -rf vendor/gms/product/app/Chrome || true && \
-  find vendor/gms -type f -iname \"Chrome.apk\" -exec rm -f {} \; || true && \
-  echo '✅ Tapos na alisin ang Chrome.' && \
 
   # 🔹 Hakbang 8: I-setup ang environment at simulan ang pag-build
   . build/envsetup.sh && \
