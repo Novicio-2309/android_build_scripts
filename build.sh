195822d256da7b79f42f6a2365c1c5b9529e71a1
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-07-14 18:24:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
      # Linisin ang lumang manifest at soong
-    rm -rf .repo/local_manifests
+    "rm -rf .repo/local_manifests &&
          # Repo init gamit ang InfinityX manifest
-    repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
+    repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
          # Clone ng local manifests mo
-    git clone https://github.com/Novicio-2309/local_manifests.git -b infinity-16-bp2a .repo/local_manifests
+    git clone https://github.com/Novicio-2309/local_manifests.git -b infinity-16-bp2a .repo/local_manifests &&
          # Clone ng signing keys at pag-generate ng keys
-    git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
-     cd vendor/infinity-priv/keys && chmod +x keys.sh && ./keys.sh
+    git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys &&
+     cd vendor/infinity-priv/keys && chmod +x keys.sh && ./keys.sh &&
      cd ../../../
          # Sync ng source code
-    /opt/crave/resync.sh
+    /opt/crave/resync.sh &&
          # Setup ng environment
-    . build/envsetup.sh
+    . build/envsetup.sh &&
      lunch infinity_LG7n-userdebug
          # Simulan ang build
-    mka bacon 
+    mka bacon"
