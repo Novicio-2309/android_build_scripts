6de36bec5af0508d9295eae92acd4a080c864d99
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-12 15:37:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,40 +1,37 @@
-     #!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
+#!/bin/bash
 set -e
 
-# 1. Linisin ang luma mong local_manifests
-rm -rf .repo/local_manifests
+crave run --no-patch -- "
+rm -rf .repo/local_manifests &&
+rm -rf device/tecno/LG7n &&
+rm -rf device/tecno/mt6789-common &&
+rm -rf device/tecno/LG7n-kernel &&
+rm -rf vendor/tecno/LG7n &&
+rm -rf vendor/tecno/mt6789-common &&
+rm -rf vendor/sony/dolby &&
+rm -rf vendor/JamesDSP &&
+rm -rf hardware/mediatek &&
+rm -rf hardware/transsion &&
+rm -rf device/mediatek/sepolicy_vndr &&
+rm -rf vendor/lineage-priv/keys &&
+rm -rf build/soong &&
+rm -rf vendor/google/gms &&
+rm -rf vendor/gms &&
 
-# 2. I-init ang repo gamit ang tamang manifest (VALID LINK)
-repo init --no-repo-verify --git-lfs \
-    -u https://github.com/ProjectInfinity-X/manifest \
-    -b 16 -g default,-mips,-darwin,-notdefault
+#Repo init
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
 
-# 3. I-clone ang local_manifests mo (SIGURADUHIN na existing ang branch!)
-git clone https://github.com/Novicio-2309/local_manifests.git \
-    -b infinity-16-bp2a .repo/local_manifests
+#Clone local manifests
+git clone https://github.com/Novicio-2309/local_manifests.git -b infinity-16-bp2a .repo/local_manifests &&
 
-# 4. I-clone ang signing key template (PUBLIC repo, should work)
-git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+#Sync the full source
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all) &&
 
-# 5. I-run ang key generator script
-cd vendor/infinity-priv/keys
-chmod +x keys.sh
-./keys.sh
-cd ../../../
+#signing keys and run setup
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
 
-# 6. Full repo sync gamit ang Crave-resync script (assumes Crave is setup)
-if [ -f /opt/crave/resync.sh ]; then
-    /opt/crave/resync.sh
-else
-    echo "[ERROR] /opt/crave/resync.sh not found!"
-    exit 1
-fi
-
-# 7. Build environment setup
-. build/envsetup.sh
-lunch infinity_LG7n-userdebug
-
-# 8. Start the build
-mka bacon
+#Setup environment and start build
+. build/envsetup.sh &&
+mistify LG7n userdebug &&
+mist b
+"
