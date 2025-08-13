c438087f881759d9df0d2715635efc8d783c4302
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-13 22:51:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,20 +3,6 @@ set -e
 
 crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
-rm -rf device/tecno/LG7n &&
-rm -rf device/tecno/mt6789-common &&
-rm -rf device/tecno/LG7n-kernel &&
-rm -rf vendor/tecno/LG7n &&
-rm -rf vendor/tecno/mt6789-common &&
-rm -rf vendor/sony/dolby &&
-rm -rf vendor/JamesDSP &&
-rm -rf hardware/mediatek &&
-rm -rf hardware/transsion &&
-rm -rf device/mediatek/sepolicy_vndr &&
-rm -rf vendor/lineage-priv/keys &&
-rm -rf build/soong &&
-rm -rf vendor/google/gms &&
-rm -rf vendor/gms &&
 
 #Repo init
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
