f0e1e7a6172b4a26f9c4647eeb5999b404f77071
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-18 00:54:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,31 +2,16 @@
 set -e
 
 crave run --no-patch -- "
+# Remove old local manifests
 rm -rf .repo/local_manifests &&
-rm -rf device/tecno/mt6789-common &&
-rm -rf device/tecno/LG7n-kernel &&
-rm -rf vendor/tecno/LG7n &&
-rm -rf vendor/tecno/mt6789-common &&
-rm -rf vendor/sony/dolby &&
-rm -rf vendor/JamesDSP &&
-rm -rf hardware/mediatek &&
-rm -rf hardware/transsion &&
-rm -rf device/mediatek/sepolicy_vndr &&
-rm -rf build/soong &&
 
-#Clone local manifests
+# Clone updated local manifest
 git clone https://github.com/Novicio-2309/local_manifests.git -b cherishOS-16 .repo/local_manifests &&
 
-#Repo init
-repo init -u https://github.com/CherishOS/android_manifest.git -b sixteen &&
+# Sync sources with new manifest
+repo sync -c -j\$(nproc --all) --force-sync --no-clone-bundle --no-tags &&
 
-#Sync the full source
-repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags &&
-
-#signing keys and run setup
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/cherish-priv/keys &&
-
-#Setup environment and start build
+# Setup environment and start build
 . build/envsetup.sh &&
 brunch cherish_LG7n
 "
