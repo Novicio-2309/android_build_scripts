7f29de9d9d94926a8e1f1c0c6fdebcbbef3e6f57
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-21 10:14:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,31 @@ set -e
 
 crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
+rm -rf device/tecno/LG7n &&
+rm -rf device/tecno/mt6789-common &&
+rm -rf device/tecno/LG7n-kernel &&
+rm -rf vendor/tecno/LG7n &&
+rm -rf vendor/tecno/mt6789-common &&
+rm -rf vendor/sony/dolby &&
+rm -rf vendor/JamesDSP &&
+rm -rf hardware/mediatek &&
+rm -rf hardware/transsion &&
+rm -rf device/mediatek/sepolicy_vndr &&
+rm -rf vendor/mediatek/ims &&
+rm -rf prebuilts/clang/host/linux-x86 
+rm -rf platform/prebuilts/clang/host/linux-x86 &&
 
 # Manifest
+# Manifest
 git clone https://github.com/Novicio-2309/local_manifests.git -b infinity-16-bp2a .repo/local_manifests &&
+
+# Initializing repo
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
+
+# Syncing repo
+repo sync --force-sync
+
+# Signingkey
 
 #Setup environment and start build
 . build/envsetup.sh &&
