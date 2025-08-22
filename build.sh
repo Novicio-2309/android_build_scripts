6ed1f5761b18dab8fe3d06df7a5e5b992943ba01
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-22 19:54:26 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,15 +4,6 @@ set -e
 crave run --no-patch -- "
 rm -rf out/soong out/target &&
 
-# Manifest
-git clone https://github.com/Novicio-2309/local_manifests.git -b infinity-16-bp2a .repo/local_manifests &&
-
-# Initializing repo
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
-
-# Syncing repo
-repo sync --force-sync &&
-
 # Signingkey
 git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
 
