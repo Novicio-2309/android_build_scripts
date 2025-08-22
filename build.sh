52a3f2309a0380d91ebbeb54d35bcb503693f85f
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-22 19:53:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,20 +2,7 @@
 set -e
 
 crave run --no-patch -- "
-rm -rf .repo/local_manifests &&
-rm -rf device/tecno/LG7n &&
-rm -rf device/tecno/mt6789-common &&
-rm -rf device/tecno/LG7n-kernel &&
-rm -rf vendor/tecno/LG7n &&
-rm -rf vendor/tecno/mt6789-common &&
-rm -rf vendor/sony/dolby &&
-rm -rf vendor/JamesDSP &&
-rm -rf hardware/mediatek &&
-rm -rf hardware/transsion &&
-rm -rf device/mediatek/sepolicy_vndr &&
-rm -rf vendor/mediatek/ims &&
-rm -rf prebuilts/clang/host/linux-x86 
-rm -rf platform/prebuilts/clang/host/linux-x86 &&
+rm -rf out/soong out/target &&
 
 # Manifest
 git clone https://github.com/Novicio-2309/local_manifests.git -b infinity-16-bp2a .repo/local_manifests &&
@@ -27,7 +14,7 @@ repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/man
 repo sync --force-sync &&
 
 # Signingkey
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-priv/keys &&
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
 
 #Setup environment and start build
 . build/envsetup.sh &&
