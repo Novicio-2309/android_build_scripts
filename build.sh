7a34f090b05452cdb67f5607e75cd227ae9cb8f6
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-23 01:16:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,12 +17,17 @@ rm -rf vendor/lineage-priv/keys &&
 rm -rf build/soong &&
 rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
-
-#Repo init
-repo init -u https://github.com/Project-Mist-OS/manifest.git -b 16 --git-lfs &&
+rm -rf prebuilts/clang/host/linux-x86 &&
+rm -rf platform/prebuilts/clang/host/linux-x8 &&
 
 #Clone local manifests
 git clone https://github.com/Novicio-2309/local_manifests.git -b mistOS16-Bp2a .repo/local_manifests &&
+
+#Repo init
+repo init -u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs
+
+# Syncing repo
+repo sync
 
 #Sync the full source
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) &&
@@ -32,6 +37,5 @@ git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-pr
 
 #Setup environment and start build
 . build/envsetup.sh &&
-mistify LG7n userdebug &&
-mist b
+brunch LG7n
 "
