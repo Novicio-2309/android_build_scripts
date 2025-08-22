0e20f6762a1740d9f772e82e6a42d884f032b873
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-08-23 01:36:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
 rm -rf prebuilts/clang/host/linux-x86 &&
 rm -rf platform/prebuilts/clang/host/linux-x8 &&
+rm -rf out
 
 #Clone local manifests
 git clone https://github.com/Novicio-2309/local_manifests.git -b voltage-16 .repo/local_manifests &&
