4897df2c5349941aa27639bc8bde5d3f79a8b2d9
Novicio Gaming <157868622+Novicio-2309@users.noreply.github.com>
2025-09-02 21:55:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ rm -rf out &&
 git clone https://github.com/Novicio-2309/local_manifests.git -b mistOS16-Bp2a .repo/local_manifests &&
 
 #Repo init
-repo init -u https://github.com/Project-Mist-OS/manifest.git -b 16 --git-lfs &&
+repo init -u https://github.com/Project-Mist-OS/manifest -b 16 --git-lfs &&
 
 #Sync the full source
 repo sync --force-sync &&
