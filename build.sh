952c7901f547b80dbc29046124ae9747981a094f
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-07 21:30:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,17 @@
 set -e
 
 crave run --no-patch -- "
+# fix blocked Android.mk (Cherish issue)
+find hardware/google/ -type f -name 'Android.mk' -exec mv {} {}.bak \; || true &&
+
+# clear build cache (safe lang, hindi kasama ccache)
 rm -rf out/soong out/target &&
-. build/envsetup.sh &&
+
+# setup environment
+export ALLOW_MISSING_DEPENDENCIES=true
+. build/envsetup.sh
+add_lunch_combo cherish_LG7n-userdebug || true
+
+# start build
 brunch LG7n
 "
