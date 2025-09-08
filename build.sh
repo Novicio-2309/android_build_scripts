0103689e1d094d2d0d14416ffa4236bcd9c72a28
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-08 23:39:09 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ set -e
 crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
 rm -rf device/tecno/LG7n &&
+rm -rf device/tecno/LH7n &&
 rm -rf device/tecno/mt6789-common &&
 rm -rf device/tecno/LG7n-kernel &&
+rm -rf device/tecno/LH7n-kernel &&
 rm -rf vendor/tecno/LG7n &&
+rm -rf vendor/tecno/LH7n &&
 rm -rf vendor/tecno/mt6789-common &&
 rm -rf vendor/sony/dolby &&
 rm -rf vendor/JamesDSP &&
@@ -25,10 +28,10 @@ rm -rf platform/prebuilts/clang/host/linux-x86 &&
 rm -rf out &&
 
 #Clone local manifests
-git clone https://github.com/Novicio-2309/local_manifests.git -b rising16-bp2a .repo/local_manifests &&
+git clone https://github.com/Novicio-2309/local_manifests.git -b derp16 .repo/local_manifests &&
 
 #Repo init
-repo init -u https://github.com/RisingOS-Revived/android -b sixteen-los --git-lfs &&
+repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16 --git-lfs &&
 
 #Sync the full source
 repo sync --force-sync &&
@@ -38,6 +41,6 @@ git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-pr
 
 #Setup environment and start build
 . build/envsetup.sh &&
-riseup LG7n userdebug &&
-rise b
+lunch lineage_LG7n-bp2a-userdebug &&
+mka derp
 "
