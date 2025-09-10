e3be206afd8258418b859f5559c6668bff667769
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-11 05:36:30 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ set -e
 
 crave run --no-patch -- "
 repo sync --force-sync &&
+rm -rf vendor/lineage-priv/keys &&
+
+# Signing key
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-priv/keys
 
 # Clean only target outputs (not full clean, safe for Crave)
 rm -rf out/target out/soong &&
