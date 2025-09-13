571da74aeaa6c9289fa38ba4b826ac4530070f50
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-13 09:58:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,47 @@
 set -e
 
 crave run --no-patch -- "
-repo sync --force-sync &&
+rm -rf .repo/local_manifests &&
+rm -rf device/tecno/LG7n &&
+rm -rf device/tecno/LH7n &&
+rm -rf device/tecno/mt6789-common &&
+rm -rf device/tecno/LG7n-kernel &&
+rm -rf device/tecno/LH7n-kernel &&
+rm -rf vendor/tecno/LG7n &&
+rm -rf vendor/tecno/LH7n &&
+rm -rf vendor/tecno/mt6789-common &&
+rm -rf vendor/sony/dolby &&
+rm -rf vendor/JamesDSP &&
+rm -rf packages/apps/ViPER4AndroidFX &&
+rm -rf hardware/mediatek &&
+rm -rf hardware/transsion &&
+rm -rf device/mediatek/sepolicy_vndr &&
 rm -rf vendor/lineage-priv/keys &&
+rm -rf vendor/derp/signing/keys &&
+rm -rf vendor/voltage-priv/keys &&
+rm -rf build/soong &&
+rm -rf vendor/google/gms &&
+rm -rf vendor/gms &&
+rm -rf external/chromium-webview &&
+rm -rf device/lineage/sepolicy &&
+rm -rf prebuilts/clang/host/linux-x86 &&
+rm -rf platform/prebuilts/clang/host/linux-x86 &&
+rm -rf out &&
 
-# Signing key
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-priv/keys
+#Clone local manifests
+git clone https://github.com/Novicio-2309/local_manifests.git -b lunaris16 .repo/local_manifests &&
 
-# Clean only target outputs (not full clean, safe for Crave)
-rm -rf out/target out/soong &&
+#Repo init
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs &&
 
-# Setup environment
+#Sync the full source
+repo sync --force-sync &&
+
+#signing keys and run setup
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-priv/keys &&
+
+#Setup environment and start build
 . build/envsetup.sh &&
-
-# Lunch your device
 lunch lineage_LG7n-bp2a-userdebug &&
-
-# Start build
 m lunaris
 "
