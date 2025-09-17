188967ed74e8e76546262e35ff503d7c03ee7034
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-17 22:05:09 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -23,9 +23,10 @@ rm -rf vendor/alpha-priv/keys &&
 rm -rf build/soong &&
 rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
+rm -rf device/lineage/sepolicy &&
 rm -rf prebuilts/clang/host/linux-x86 &&
 rm -rf platform/prebuilts/clang/host/linux-x86 &&
-rm -rf out
+rm -rf out &&
 
 # Initialize repo
 repo init -u https://github.com/alphadroid-project/manifest -b alpha-15.2 --git-lfs &&
