48fb794ef8a4641d830755bd76841ed14b88f4d6
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-18 16:20:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ set -e
 crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
 rm -rf device/tecno/LH7n &&
+rm -rf device/tecno/LG7n &&
 rm -rf device/tecno/mt6789-common &&
 rm -rf device/tecno/LG7n-kernel &&
+rm -rf device/tecno/LH7n-kernel &&
 rm -rf vendor/tecno/LG7n &&
+rm -rf vendor/tecno/LH7n &&
 rm -rf vendor/tecno/mt6789-common &&
 rm -rf vendor/sony/dolby &&
 rm -rf packages/apps/ViPER4AndroidFX &&
@@ -15,6 +18,7 @@ rm -rf hardware/mediatek &&
 rm -rf hardware/transsion &&
 rm -rf device/mediatek/sepolicy_vndr &&
 rm -rf vendor/infinity-priv/keys &&
+rm -rf vendor/alpha-priv/keys &&
 rm -rf build/soong &&
 rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
