df75d886cee33a679717f397fd533df79f41cb5a
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-18 16:20:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,6 +22,7 @@ rm -rf vendor/alpha-priv/keys &&
 rm -rf build/soong &&
 rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
+rm -rf device/lineage/sepolicy &&
 rm -rf prebuilts/clang/host/linux-x86 &&
 rm -rf platform/prebuilts/clang/host/linux-x86 &&
 rm -rf out &&
