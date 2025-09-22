1a167a17e4423d41f42034716668efc809d82657
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-09-22 20:45:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,6 +20,7 @@ rm -rf vendor/voltage-priv/keys &&
 rm -rf build/soong &&
 rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
+rm -rf external/chromium-webview &&
 rm -rf prebuilts/clang/host/linux-x86 &&
 rm -rf platform/prebuilts/clang/host/linux-x86 &&
 rm -rf out &&
