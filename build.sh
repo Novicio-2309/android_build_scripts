5a67ebef0acde05baef033f2391ff233cdc75a96
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-10-13 16:32:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,44 +3,19 @@ set -e
 
 crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
-rm -rf device/tecno/LG7n &&
-rm -rf device/tecno/mt6789-common &&
-rm -rf device/tecno/LG7n-kernel &&
-rm -rf vendor/tecno/LG7n &&
-rm -rf vendor/tecno/mt6789-common &&
-rm -rf vendor/sony/dolby &&
-rm -rf vendor/JamesDSP &&
-rm -rf packages/apps/ViPER4AndroidFX &&
-rm -rf hardware/mediatek &&
-rm -rf hardware/transsion &&
-rm -rf device/mediatek/sepolicy_vndr &&
-rm -rf vendor/lineage-priv/keys &&
-rm -rf vendor/evolution-priv/keys &&
-rm -rf vendor/derp/signing/keys &&
-rm -rf vendor/voltage-priv/keys &&
-rm -rf build/soong &&
-rm -rf vendor/google/gms &&
-rm -rf vendor/gms &&
-rm -rf device/lineage/sepolicy &&
-rm -rf vendor/official_devices &&
-rm -rf external/chromium-webview &&
-rm -rf prebuilts/clang/host/linux-x86 &&
-rm -rf platform/prebuilts/clang/host/linux-x86 &&
-rm -rf out &&
 
 #Clone local manifests
-git clone https://github.com/Novicio-2309/local_manifests.git -b witaqua16 .repo/local_manifests &&
+git clone https://github.com/Novicio-2309/android_device_xiaomi_amethyst &&
 
 #Repo init
-repo init -u https://github.com/WitAqua/manifest.git -b 16.0 --git-lfs &&
+repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-14.1 &&
 
 #Sync the full source
 repo sync --force-sync &&
 
-#signing keys and run setup
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/witaqua-priv/keys &&
-
 #Setup environment and start build
+export ALLOW_MISSING_DEPENDENCIES=true &&
 . build/envsetup.sh &&
-brunch LG7n
+lunch twrp_amethyst-eng &&
+mka recoveryimage
 "
