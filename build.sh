66141f2168f61a69a1ece5bc11a308d0aed32c6c
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-10-17 23:59:51 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
 rm -rf external/chromium-webview &&
 rm -rf device/lineage/sepolicy &&
+rm -rf vendor/official_devices &&
 rm -rf prebuilts/clang/host/linux-x86 &&
 rm -rf platform/prebuilts/clang/host/linux-x86 &&
 rm -rf out &&
