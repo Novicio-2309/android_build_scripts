e01141577c9fc9aad21ac94f41a56ae55ff26554
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2025-11-07 17:53:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -26,7 +26,6 @@ rm -rf vendor/google/gms &&
 rm -rf vendor/gms &&
 rm -rf prebuilts/clang/host/linux-x86 &&
 rm -rf platform/prebuilts/clang/host/linux-x86 &&
-rm -rf out &&
 
 #Clone local manifests
 git clone https://github.com/Novicio-2309/local_manifests.git -b crdroid16 .repo/local_manifests &&
