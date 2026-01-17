eac7994d0623d6544b9e56b0caa6e8b8c0b87dca
PornhubProject <157868622+Novicio-2309@users.noreply.github.com>
2026-01-17 22:07:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
 
 #Clone local manifests
-git clone https://github.com/Novicio-2309/device_xiaomi_amethysu-twrp device/xiaomi/amethyst &&
+https://github.com/Novicio-2309/device_xiaomi_amethyst-recovery &&
 
 #Repo init
-repo init -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-14.1 &&
+repo init --depth=1 -u https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-14.1 &&
 
 #Sync the full source
 repo sync --force-sync &&
