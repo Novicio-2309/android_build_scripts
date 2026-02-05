16154731eabfdcda1658ebe3cdc3288fd892c842
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-02-05 15:38:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,19 +32,19 @@ rm -rf vendor/official_devices &&
 rm -rf prebuilts/clang/host/linux-x86/clang-r530567/bin &&
 
 #Repo init
-repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 16.2 --git-lfs &&
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
 
 #Clone local manifests
-git clone https://github.com/Novicio-2309/local_manifests.git -b Derp16Qpr2 .repo/local_manifests &&
+git clone https://github.com/Novicio-2309/local_manifests.git -b InfinityX16Qpr2 .repo/local_manifests &&
 
 #Sync the full source
 /opt/crave/resync.sh &&
 
 #signing keys and run setup
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-priv/keys &&
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
 
 #Setup environment and start build
 . build/envsetup.sh &&
-lunch lineage_LG7n-bp4a-userdebug &&
-mka derp
+lunch infinity_LG7n-userdebug &&
+m bacon
 "
