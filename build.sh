40436fe29d52d29be7fd7e9b656df2e453e9591e
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-06-02 16:39:28 +0800

    Clean up device and vendor directories
    Remove various device and vendor directories before cloning local manifests.

--- build.sh
+++ build.sh
@@ -3,34 +3,6 @@ set -e
 
 crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
-rm -rf device/tecno/LG7n &&
-rm -rf device/tecno/LH7n &&
-rm -rf device/tecno/mt6789-common &&
-rm -rf device/tecno/LG7n-kernel &&
-rm -rf device/tecno/LH7n-kernel &&
-rm -rf vendor/tecno/LG7n &&
-rm -rf vendor/tecno/LH7n &&
-rm -rf vendor/tecno/mt6789-common &&
-rm -rf vendor/sony/dolby &&
-rm -rf vendor/JamesDSP &&
-rm -rf packages/apps/ViPER4AndroidFX &&
-rm -rf hardware/mediatek &&
-rm -rf packages/apps/ParanoidSense &&
-rm -rf hardware/transsion &&
-rm -rf device/mediatek/sepolicy_vndr &&
-rm -rf vendor/lineage-priv/keys &&
-rm -rf vendor/derp/signing/keys &&
-rm -rf vendor/voltage-priv/keys &&
-rm -rf build/soong &&
-rm -rf vendor/google/gms &&
-rm -rf vendor/google/overlays &&
-rm -rf vendor/gms &&
-rm -rf external/chromium-webview &&
-rm -rf device/lineage/sepolicy &&
-rm -rf vendor/official_devices &&
-rm -rf packages/apps/Aperture &&
-rm -rf prebuilts/clang/host/linux-x86 &&
-rm -rf platform/prebuilts/clang/host/linux-x86 &&
 
 #Clone local manifests
 git clone https://github.com/Novicio-2309/local_manifests.git -b Lunaris16Qpr2 .repo/local_manifests &&
