a449e4a84e2f5a89cac041670be89d421652fde2
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-06 15:25:58 +0800

    Change repo init command to use depth=1

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ crave run --no-patch -- "
 rm -rf .repo/local_manifests &&
 
 #Repo init
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
 
 #Clone local manifests
 git clone https://github.com/Novicio-2309/local_manifests.git -b Infinity16-amethyst .repo/local_manifests &&
