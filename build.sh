5eee828930705b71e9271cc6797a9b4a0abf173e
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-10 20:00:42 +0800

    Fix branch name and clean up build.sh
    Updated the branch name for local manifests and removed the Dolby clone command.

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ rm -rf hardware/dolby &&
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
 
 #Clone local manifests
-git clone https://github.com/Novicio-2309/local_manifests.git -b infinitySex-amethyst .repo/local_manifests &&
+git clone https://github.com/Novicio-2309/local_manifests.git -b Infinity16 .repo/local_manifests &&
 
 #Sync the full source
 /opt/crave/resync.sh &&
@@ -24,9 +24,6 @@ git clone https://github.com/Novicio-2309/local_manifests.git -b infinitySex-ame
 #reclone sepolicy
 rm -rf device/qcom/sepolicy_vndr/sm8650 &&
 git clone https://github.com/amethyst-playground/android_device_qcom_sepolicy_vndr.git -b lineage-23.2-caf-sm8650 device/qcom/sepolicy_vndr/sm8650 --depth=1 &&
-
-# Dolby
-git clone --depth=1 https://github.com/LineageOS-extra/android_hardware_dolby.git -b lineage-23.2 hardware/dolby &&
 
 #key
 git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
