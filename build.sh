6630c4872dc4ccf92947eecdc4392bae42e7547e
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-14 03:48:50 +0800

    Update signing key repository path in build script

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ rm -rf device/qcom/sepolicy_vndr/sm8650 &&
 git clone https://github.com/amethyst-playground/android_device_qcom_sepolicy_vndr.git -b lineage-23.2-caf-sm8650 device/qcom/sepolicy_vndr/sm8650 --depth=1 &&
 
 #key
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/yaap/signing/keys &&
 
 #Setup environment and start build
 source build/envsetup.sh &&
