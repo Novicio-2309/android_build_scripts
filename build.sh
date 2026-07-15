06929b70b20ebbd7dcb0129674ecddfc730477fb
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-15 12:49:46 +0800

    Remove LunarisDolby app and ant_native Android.mk
    Remove LunarisDolby app and related Android.mk file from external.

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ rm -rf hardware/dolby &&
 rm -rf packages/apps/ViPER4AndroidFX &&
 rm -rf prebuilts/gcc/linux-x86/arm/arm-linux- androideabi-4.9 &&
 rm -rf external/ant-wireless &&
+rm -rf packages/apps/LunarisDolby &&
+rm -rf external/ant-wireless/ant_native/Android.mk &&
 
 #Repo init
 repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
