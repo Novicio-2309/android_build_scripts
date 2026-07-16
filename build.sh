071813dc4a3a399254afd3099e8e3b0b77bc4ce7
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-16 20:12:42 +0800

    Update build script for new repository and branches

--- build.sh
+++ build.sh
@@ -22,10 +22,10 @@ rm -rf vendor/themes &&
 
 
 #Repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
+repo init --depth=1 -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle &&
 
 #Clone local manifests
-git clone https://github.com/Novicio-2309/local_manifests.git -b Infinity16 .repo/local_manifests &&
+git clone https://github.com/Novicio-2309/local_manifests.git -b crdroid .repo/local_manifests &&
 
 #Sync the full source
 /opt/crave/resync.sh &&
@@ -35,10 +35,9 @@ rm -rf device/qcom/sepolicy_vndr/sm8650 &&
 git clone https://github.com/amethyst-playground/android_device_qcom_sepolicy_vndr.git -b lineage-23.2-caf-sm8650 device/qcom/sepolicy_vndr/sm8650 --depth=1 &&
 
 #key
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-priv/keys &&
 
 #Setup environment and start build
 . build/envsetup.sh &&
-lunch infinity_amethyst-userdebug &&
-m bacon
+brunch amethyst
 "
