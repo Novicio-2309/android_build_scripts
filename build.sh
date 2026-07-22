f669ea1e2c810345aff558c02e75621831b828d7
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-22 14:53:03 +0800

    Modify build script for new repo and sepolicy updates
    Updated repository URLs and added automatic insertion for sepolicy ignore.cil files.

--- build.sh
+++ build.sh
@@ -21,12 +21,11 @@ rm -rf yaap &&
 rm -rf hardware/lineage/build/soong/generator &&
 rm -rf vendor/themes &&
 
-
 #Repo init
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault &&
+repo init --depth=1 -u https://github.com/AviumUI/android_manifests -b avium-16.2 --git-lfs &&
 
 #Clone local manifests
-git clone https://github.com/Novicio-2309/local_manifests.git -b Infinity16 .repo/local_manifests &&
+git clone https://github.com/Novicio-2309/local_manifests.git -b Avium16 .repo/local_manifests &&
 
 #Sync the full source
 /opt/crave/resync.sh &&
@@ -36,10 +35,25 @@ rm -rf device/qcom/sepolicy_vndr/sm8650 &&
 git clone https://github.com/BluedMC-Amethyst/device_qcom_sepolicy_vndr.git -b lineage-23.2-caf-sm8650 device/qcom/sepolicy_vndr/sm8650 --depth=1 &&
 
 #key
-git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
+git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/lineage-priv/keys &&
+
+# Automatic insertion for sepolicy ignore.cil files
+echo 'Adding devicesettings_app and settingslib_prop to ignore.cil files...' &&
+
+# For 34.0 ignore.cil
+if [ -f system/sepolicy/private/compat/34.0/34.0.ignore.cil ]; then
+    grep -q 'devicesettings_app' system/sepolicy/private/compat/34.0/34.0.ignore.cil || echo -e '\ndevicesettings_app' >> system/sepolicy/private/compat/34.0/34.0.ignore.cil
+    grep -q 'settingslib_prop' system/sepolicy/private/compat/34.0/34.0.ignore.cil || echo -e 'settingslib_prop' >> system/sepolicy/private/compat/34.0/34.0.ignore.cil
+fi &&
+
+# For 202404 ignore.cil
+if [ -f system/sepolicy/private/compat/202404/202404.ignore.cil ]; then
+    grep -q 'devicesettings_app' system/sepolicy/private/compat/202404/202404.ignore.cil || echo -e '\ndevicesettings_app' >> system/sepolicy/private/compat/202404/202404.ignore.cil
+    grep -q 'settingslib_prop' system/sepolicy/private/compat/202404/202404.ignore.cil || echo -e 'settingslib_prop' >> system/sepolicy/private/compat/202404/202404.ignore.cil
+fi &&
 
 #Setup environment and start build
 . build/envsetup.sh &&
-lunch infinity_amethyst-userdebug &&
+lunch lineage_amethyst-bp4a-userdebug &&
 m bacon
 "
