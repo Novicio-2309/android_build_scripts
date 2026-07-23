466f2fc2238e76acb4c69610c4dbb202a3549158
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-07-23 13:29:14 +0800

    Implement automatic ignore.cil updates for devicesettings_app
    Add automatic insertion of device settings to ignore.cil files for compatibility versions.

--- build.sh
+++ build.sh
@@ -38,6 +38,21 @@ git clone https://github.com/BluedMC-Amethyst/device_qcom_sepolicy_vndr.git -b l
 #key
 git clone --depth=1 https://github.com/Novicio-2309/signingkey vendor/infinity-priv/keys &&
 
+# Automatic insertion for sepolicy ignore.cil files
+echo 'Adding devicesettings_app and settingslib_prop to ignore.cil files...' &&
+
+# For 34.0 ignore.cil
+if [ -f system/sepolicy/private/compat/34.0/34.0.ignore.cil ]; then
+    grep -q 'devicesettings_app' system/sepolicy/private/compat/34.0/34.0.ignore.cil || echo -e '\ndevicesettings_app' >> system/sepolicy/private/compat/34.0/34.0.ignore.cil
+    grep -q 'settingslib_prop' system/sepolicy/private/compat/34.0/34.0.ignore.cil || echo -e 'settingslib_prop' >> system/sepolicy/private/compat/34.0/34.0.ignore.cil
+fi &&
+
+# For 202404 ignore.cil
+if [ -f system/sepolicy/private/compat/202404/202404.ignore.cil ]; then
+    grep -q 'devicesettings_app' system/sepolicy/private/compat/202404/202404.ignore.cil || echo -e '\ndevicesettings_app' >> system/sepolicy/private/compat/202404/202404.ignore.cil
+    grep -q 'settingslib_prop' system/sepolicy/private/compat/202404/202404.ignore.cil || echo -e 'settingslib_prop' >> system/sepolicy/private/compat/202404/202404.ignore.cil
+fi &&
+
 #Setup environment and start build
 . build/envsetup.sh &&
 lunch infinity_amethyst-userdebug &&
